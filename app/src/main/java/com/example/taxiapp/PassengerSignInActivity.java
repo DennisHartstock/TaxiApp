5d52cee49f1e2e8a068eee588584592114157ac6
DennsHartstock <dennis.hartstock@web.de>
2022-02-09 05:32:56 +0100

    implemented validation userInput

--- app/src/main/java/com/example/taxiapp/PassengerSignInActivity.java
+++ app/src/main/java/com/example/taxiapp/PassengerSignInActivity.java
@@ -3,12 +3,94 @@ package com.example.taxiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class PassengerSignInActivity extends AppCompatActivity {
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputName;
+    private TextInputLayout textInputPassword;
+    private TextInputLayout textInputConfirmPassword;
+    private Button signUpButton;
+    private TextView toggleSignUpLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_sign_in);
+
+        textInputEmail = findViewById(R.id.textInputEmail);
+        textInputName = findViewById(R.id.textInputName);
+        textInputPassword = findViewById(R.id.textInputPassword);
+        textInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
+        signUpButton = findViewById(R.id.signUpButton);
+        toggleSignUpLoginTextView = findViewById(R.id.toggleSignUpLoginTextView);
+    }
+
+    private boolean validateEmail() {
+        String emailInput = textInputEmail.getEditText().getText().toString().trim();
+
+        if (emailInput.isEmpty()) {
+            textInputEmail.setError("Please input your email");
+            return false;
+        } else {
+            textInputEmail.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateName() {
+        String nameInput = textInputName.getEditText().getText().toString().trim();
+
+        if (nameInput.isEmpty()) {
+            textInputName.setError("Please input your name");
+            return false;
+        } else if (nameInput.length() > 15) {
+            textInputName.setError("Name is too long");
+            return false;
+        } else {
+            textInputName.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+        String confirmPasswordInput = textInputConfirmPassword.getEditText().getText().toString().trim();
+
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Please input your password");
+            return false;
+        } else if (passwordInput.length() < 6) {
+            textInputPassword.setError("Password must have at least 6 chars");
+            return false;
+        } else if (!passwordInput.equals(confirmPasswordInput)) {
+            textInputPassword.setError(null);
+            textInputConfirmPassword.setError("Password is not match");
+            return false;
+        } else {
+            textInputPassword.setError(null);
+            textInputConfirmPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void signUp(View view) {
+        if (!validateEmail() | !validateName() | !validatePassword()) {
+            return;
+        }
+
+        String userInput = "Email: " + textInputEmail.getEditText().getText().toString().trim() + "\n"
+                + "Name: "  + textInputName.getEditText().getText().toString().trim() + "\n"
+                + "Password: " + textInputPassword.getEditText().getText().toString().trim();
+
+        Toast.makeText(this, userInput, Toast.LENGTH_LONG).show();
+    }
+
+    public void toggleSignUpLogin(View view) {
     }
 }
